80067c7fbcc2ca16da3858ec30802222cb42cf32
m-dantas <mauricio.dantascp@gmail.com>
2020-09-01 11:43:47 -0300

    fix: incluindo setup

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+const baseUrl = '/sispop/'
+const apiBaseUrl = 'https://servicos.spurbanismo.sp.gov.br/triangulo/api/apivistorias/'
+
+export default {
+  mode: 'spa',
+  target: 'static',
+  env: {
+    apiBaseUrl
+  },
+  head: {
+    htmlAttrs: { lang: 'pt-br' },
+    title: 'SISPOP da São Paulo Urbanismo',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: `${baseUrl}favicon.ico` }
+    ]
+  },
+  styleResources: {
+    scss: ['@/assets/style/variables.scss', '@/assets/style/theme.scss']
+  },
+  plugins: [],
+  components: true,
+  buildModules: [
+    '@nuxtjs/eslint-module'
+  ],
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
+  ],
+  router: {
+    base: baseUrl,
+    fallback: true
+  },
+  pwa: {
+    manifest: {
+      name: 'SISPOP da São Paulo Urbanismo',
+      lang: 'ptbr',
+      short_name: 'São Paulo Urbanismo'
+    }
+  }
+}
